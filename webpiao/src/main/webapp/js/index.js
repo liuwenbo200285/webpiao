9a967158e9de0754ef4a95745624323f3f971a41
liuwenbo200285 <liuwenbo200285@gmail.com>
2014-01-03 00:16:50 +0800

    调整登录位置以及验证码刷新

--- webpiao/src/main/webapp/js/index.js
+++ webpiao/src/main/webapp/js/index.js
@@ -1,35 +1,104 @@
-Ext.application({
-		name : "HelloExt",
-		launch : function() {
-			Ext.create('Ext.panel.Panel', {
-				width :1000,
-				height :700,
-				layout : 'border',
-				items : [{
-					region : 'south',
-					xtype : 'panel',
-					height : 20,
-					split : false,
-					html : '欢迎登录!',
-					margins : '0 5 5 5'
-				}, {
-					title : 'West Region is collapsible',
-					region : 'west',
-					xtype : 'panel',
-					margins : '5 0 0 5',
-					width : 200,
-					collapsible : true,
-					id : 'west-region-container',
-					layout : 'fit'
-				}, {
-					title : 'Center Region',
-					region : 'center',
-					xtype : 'panel',
-					layout : 'fit',
-					margins : '5 5 0 0',
-					html : '在Extjs4中，center区域必须指定，否则会报错。'
-				} ],
-				renderTo : Ext.getBody()
-			});
-		}
-	});
+Ext.onReady(function () {
+            //初始化标签中的Ext:Qtip属性。
+            Ext.QuickTips.init();
+            Ext.form.Field.prototype.msgTarget = 'side';
+            var win = new Ext.Window({
+                title: '宜搜游戏运营管理后台----登录',
+                width: 476,
+                x:450,
+                y:100,
+                height: 290,
+                resizable:false,
+                modal: true,
+                closable: false,
+                maximizable: false,
+                minimizable: false,
+                items:[logoPanel,form]
+            });
+            win.show();
+        });
+var required = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>';
+var form = Ext.widget('form', {
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+    border: false,
+    bodyStyle: 'padding:0px 100px 0px 100px',
+    fieldDefaults: {
+        labelAlign: 'left',
+        labelWidth: 60,
+        labelStyle: 'font-weight:bold'
+    },
+    items: [{
+        xtype: 'textfield',
+        fieldLabel: '用户名',
+        width: 100,
+        afterLabelTextTpl: required,
+        allowBlank: false
+    }, {
+        xtype: 'textfield',
+        fieldLabel: '密码',
+        width: 100,
+        afterLabelTextTpl: required,
+        inputType:'password',
+        allowBlank: false
+    },{
+        xtype: 'fieldcontainer',
+        labelStyle: 'font-weight:bold;padding:0;',
+        layout: 'hbox',
+        fieldDefaults: {
+            labelAlign: 'left'
+        },
+        items: [{
+            flex: 1,
+            name: 'firstName',
+            xtype: 'textfield',
+            width: 100,
+            afterLabelTextTpl: required,
+            fieldLabel: '验证码',
+            allowBlank: false
+        },{     
+            xtype: 'box', //或者xtype: 'component',     
+            width: 80, //图片宽度     
+            height: 30, //图片高度     
+            autoEl: {
+            	id:'rangCode',
+                tag: 'img',    //指定为img标签     
+                src: 'http://118.244.198.75:8080/gameAdmin/module/common/VerifyCode.jsp'    //指定url路径     
+            },
+            listeners: {
+                render: function(component){
+                        component.getEl().on('click', function(e){
+                        	Ext.get("component-1014").dom.src='http://118.244.198.75:8080/gameAdmin/module/common/VerifyCode.jsp';
+                        });
+                }
+           }
+        }]
+    }],
+    buttonAlign: 'center',
+    buttons: [{
+        text: '登录',
+        handler: function() {
+            this.up('form').getForm().reset();
+            this.up('window').hide();
+        }
+    }, {
+        text: '重置',
+        handler: function() {
+            if (this.up('form').getForm().isValid()) {
+                this.up('form').getForm().reset();
+                this.up('window').hide();
+                Ext.MessageBox.alert('Thank you!', 'Your inquiry has been sent. We will respond as soon as possible.');
+            }
+        }
+    }]
+});
+var logoPanel = {
+        xtype: 'panel',
+        border: false,
+        width: '100%',
+        height: 100,
+        padding: '1px',
+        html: "<img src='http://w4.easou.com/index_2012_web.png' alt='软件LOGO' height='100%' width='100%'/>"
+    };
